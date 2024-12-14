fe8a3932550ff03b6dee40a7e311223cddfb01c9
PrajilaShehaniGit <prajilashehani@gmail.com>
2024-12-14 22:33:00 +0530

    Revert "tests"
    This reverts commit e96dcee9c49387893fd4e7e5f850be8f2cbb39d6.

--- tests/VerifySuccessfulLoginWithValidCredentials.ts
+++ tests/VerifySuccessfulLoginWithValidCredentials.ts
@@ -1,12 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-const baseURL = "https://onlinelibrary.wiley.com/";
 const email = "prajilashehani@gmail.com";
 const password = "Prajila2024";
+const baseURL = "https://wiley.scienceconnect.io/login";
 
 test("VerifyLoginRequestWithValidCredentials", async ({ request }) => {
   await test.step("Send a POST request to the login endpoint with valid credentials", async () => {
-    const response = await request.post(`${baseURL}login`, {
+    const response = await request.post(baseURL, {
       data: {
         userEmail: email,
         password: password,
@@ -15,6 +15,23 @@ test("VerifyLoginRequestWithValidCredentials", async ({ request }) => {
 
     expect(response.status()).toBe(200);
     const responseBody = await response.json();
-    expect(responseBody).toHaveProperty("userToken");
+    expect(responseBody).toHaveProperty("sessionToken");
   });
 });
+
+test("VerifyForgotPasswordEndpoint", async ({ request }) => {
+  const forgotPasswordURL = "https://wiley.scienceconnect.io/login/password";
+
+  await test.step("Send a POST request to the forgot password endpoint", async () => {
+    const response = await request.post(forgotPasswordURL, {
+      data: {
+        email: email, // Replace with a registered email
+      },
+    });
+
+    expect(response.status()).toBe(200);
+    const responseBody = await response.json();
+    expect(responseBody).toHaveProperty("message", "Password reset email sent.");
+  });
+});
+
